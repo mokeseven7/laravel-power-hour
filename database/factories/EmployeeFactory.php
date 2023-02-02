2858e372cc2e8df8fa1af5fbc59062a883aa37cd
Michael McGrath <mokeseven7@gmail.com>
2023-02-02 16:53:51 -0700

    (feature)-add-database-and-testing-facilities

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'employee_id' => fake()->numberBetween(10, 99) . '-' . fake()->toUpper(fake()->word()),
+        ];
+    }
+}
--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -16,6 +16,15 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+	
+	/**
+         * The \Illuminate\Foundation\Console\Kernel class, is what the Illuminate\Support\Facades\Artisan facade exposes
+         * But you cannot resolve facades via the container, which is of course by nature of that design pattern.
+         * The below method allows us retain the speed of sqlite in memory testing.
+         * But without having to use the "DatabaseMigrations" trait, which can only rebuild the entire database after every test. 
+         */
+        $artisan = $app->make(\Illuminate\Foundation\Console\Kernel::class);
+        $artisan->call('migrate:fresh');
 
         return $app;
     }
